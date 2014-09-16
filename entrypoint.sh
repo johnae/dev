40f2c105ba64bf9b48679e1e449b507fa0343630
John Axel Eriksson <john@insane.se>
2014-09-17 01:23:52 +0200

    Do the env var export much better

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-## ensure all set docker env vars (that begin with DOCKER_) are written to disk in the proper place
+## ensure all set docker env vars (that begin with EXPORT_) are written to disk in the proper place
 
 rm -f /etc/profile.d/docker-env.sh
 touch /etc/profile.d/docker-env.sh
@@ -9,11 +9,14 @@ for LINE in $(env); do
   OIFS=$IFS
   IFS="="
   arr=($LINE)
-  if [[ ${arr[0]} == DOCKER_* ]]; then
-    echo "export ${arr[0]}=${arr[1]}" >> /etc/profile.d/docker-env.sh
+  if [[ ${arr[0]} == EXPORT_* ]]; then
+    VARNAME=$(echo ${arr[0]} | sed 's/EXPORT_//g')
+    echo "export $VARNAME=${arr[1]}" >> /etc/profile.d/docker-env.sh
   fi
   IFS=$OIFS
 done
+
+
 
 chmod +x /etc/profile.d/docker-env.sh
 
